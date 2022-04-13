fb956128e7fb1eb9baa9995ba509e2de6b1be0de
Vitor Antoni <v.silva.antoni@gmail.com>
2022-04-13 20:17:18 -0300

    Adição do código
    Nesta versão, atualizei o código main, melhorando-o

--- Gerador-senha.py
+++ Gerador-senha.py
@@ -1,12 +1,45 @@
 import random
-tamanho = 8     # 8 caracteres
+import Functions
+import Colors
+
 letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 letrasM = 'abcdefghijklmnopqrstuvwxyz'
 num = '1234567890'
 simbolos = '~`! @#$%^&*()_-+={]|\:;<,>.?/'
-digitos = letras + letrasM + num + simbolos
-senha = ''
-for c in range(tamanho):
-    senha += random.choice(digitos)     # Randomiza os digitos
-random.sample(senha, len(senha))    # Embaralha a senha gerada acima
-print(f'Sua senha é: {senha} ')
+
+digitos = ""
+
+## Definição de tamanho
+tamanho = int(input(f"Digite o tamanho da senha {Colors.corVermelho()}(mín: 8){Colors.limpar()}: "))
+conferenciaTamanho = Functions.conferirTamanho(tamanho)
+
+## Inclusão de letras na senha
+incluirLetras = Functions.verificacaoSN("Incluir letras[S/N]? ")
+if incluirLetras in "S":
+    digitos += Functions.randomizar(letras, conferenciaTamanho)
+
+
+## Inclusão de letras maiúsculas na senha
+incluirLetrasM = Functions.verificacaoSN("Incluir letras maiúsculas[S/N]? ")
+if incluirLetrasM in "S":
+    digitos += Functions.randomizar(letrasM, conferenciaTamanho)
+
+
+## Inclusão de números na senha
+incluirNumeros = Functions.verificacaoSN("Incluir números[S/N]? ")
+if incluirNumeros in "S":
+    digitos += Functions.randomizar(num, conferenciaTamanho)
+
+
+## Inclusão de símbolos na senha
+incluirSimbolos = Functions.verificacaoSN("Incluir símbolos[S/N]? ")
+if incluirSimbolos in "S":
+    digitos += Functions.randomizar(simbolos, conferenciaTamanho)
+
+
+## Embaralhar a senha
+digitos = random.sample(digitos, conferenciaTamanho)   # Gera uma *lista* embaralhada
+
+print("\nSua senha gerada é: ", end="")
+for c in digitos:
+    print(c, end="")
