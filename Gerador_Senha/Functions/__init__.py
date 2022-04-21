534834897a88372478ecb85a681bacb826da1154
Vitor Antoni <v.silva.antoni@gmail.com>
2022-04-20 23:35:09 -0300

    Criação da pasta Gerador_Senha
    Esta pasta foi criada para uma melhor organização do projeto!

---
+++ Gerador_Senha/Functions/__init__.py
@@ -0,0 +1,34 @@
+def conferirTamanho(tam):
+    try:
+        import Colors
+        while tam < 8:
+            print(f"Senha {Colors.corVermelho()}fraca{Colors.limpar()} demais!")
+            tam = int(input(f"Digite o tamanho da senha {Colors.corVermelho()}(min: 8){Colors.limpar()}: "))
+    
+    except Exception as error:
+        print(f"Houve um erro em conferir o tamanho: {error.__class__}")
+    
+    else:
+        if tam >= 8 and tam < 10:
+            print(f"{Colors.corAmarelo()}Tamanho OK!{Colors.limpar()}\n")
+        else:
+            print(f"{Colors.corVerde()}Tamanho excelente!{Colors.limpar()}\n")
+        return tam
+
+
+def randomizar(variavel, tam=8):
+    import random
+
+    gerado = ""
+    for c in range(0, tam):
+        gerado += random.choice(variavel)
+    return gerado
+
+
+def verificacaoSN(pergunta=""):
+    verf = str(input(pergunta)).strip().upper()[0]
+    
+    while verf not in "SN":
+        print("Digite novamente...")
+        verf = str(input(pergunta)).strip().upper()[0]
+    return verf
