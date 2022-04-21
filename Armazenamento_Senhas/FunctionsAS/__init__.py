150c68c7079504a5621316e80b5beead751e0ba5
Vitor Antoni <v.silva.antoni@gmail.com>
2022-04-20 23:52:30 -0300

    Uptdate __init__.py

--- Armazenamento_Senhas/FunctionsAS/__init__.py
+++ Armazenamento_Senhas/FunctionsAS/__init__.py
@@ -1,7 +1,18 @@
+from Gerador_Senha import Colors
+
 def ArquivoExiste(nome_arquivo):
     try:
         arquivo = open(nome_arquivo + ".txt", "rt")
-    except Exception as error:
+    except:
         return False
     else:
         return True
+
+
+def CriaArquivo(nome_arquivo):
+    try: 
+        arquivo = open(nome_arquivo + ".txt", "wt+")
+    except Exception as error:
+        print(f"Houve um erro para criar o arquivo: {Colors.corVermelho}{error}{Colors.limpar()}")
+    else:
+        print("{Colors.corVerde()}Sucesso na criação do arquivo!{Colors.limpar()}")
