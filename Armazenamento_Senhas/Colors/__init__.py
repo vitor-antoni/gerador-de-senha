21caeb71da30b89e430851609a4bf910b41d5dea
Vitor Antoni <v.silva.antoni@gmail.com>
2022-04-25 20:04:50 -0300

    Criação da cor violeta

--- Armazenamento_Senhas/Colors/__init__.py
+++ Armazenamento_Senhas/Colors/__init__.py
@@ -10,6 +10,11 @@ def corAmarelo():
     cor = {"amarelo": "\033[1;33m"}
     return f"{cor['amarelo']}"
 
+def corVioleta():
+    cor = {"violeta": "\033[1;35m"}
+    return f"{cor['violeta']}"
+
+
 def limpar():
     clean = {"limpar": "\033[m"}
     return f"{clean['limpar']}"
