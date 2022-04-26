be7528466d04da2d04c3488adda15a3954ed49ce
Vitor Antoni <v.silva.antoni@gmail.com>
2022-04-25 22:01:37 -0300

    Update main.py

--- Armazenamento_Senhas/main.py
+++ Armazenamento_Senhas/main.py
@@ -1,7 +1,7 @@
 import FunctionsAS
 import Colors
 
-print(f"O que você deseja realizar?\n[ 1 ] Armazenar senha\n[ 2 ] Editar senha armazenada\n[ 3 ] Deletar senha armazenada\n[ 4 ] Visualizar senha armazenada\n[ 5 ] Para {Colors.corVermelho()}fechar{Colors.limpar()} o programa")
+print(f"\n\nO que você deseja realizar?\n[ 1 ] Armazenar senha\n[ 2 ] Editar senha armazenada\n[ 3 ] Deletar senha armazenada\n[ 4 ] Visualizar senha armazenada\n[ 5 ] Para {Colors.corVermelho()}fechar{Colors.limpar()} o programa")
 
 Acao = FunctionsAS.inteiro(f"\nDigite a ação desejada[{Colors.corVerde()}1-5{Colors.limpar()}]: ")
 while Acao > 5 or Acao < 1:
